208d8d05b2e39a13d2644a3d26b179e0c22755f0
KimberleyBonix <kimberley.bonix@oclock.school>
2023-08-25 23:20:52 +0200

    :recycle: refactor: controller students of promo page

--- app/controllers/studentController.js
+++ app/controllers/studentController.js
@@ -1,29 +1,25 @@
 const promos = require("../../data/promos.json"); // [{}, {}, {}]
 const students = require("../../data/students.json"); // [{}, {}, {}]
 
+const db = require('../database/client');
+
 const studentController = {
   renderAllStudentsOfPromoPage(req, res, next) {
-    // Récupérer l'ID de la promo demandée --> promoId
-    const promoId = parseInt(req.params.id); // ex: 663
+    const promoId = Number(req.params.id);
 
-    // Vérifier si la promo existe. Si elle n'existe pas, 404 !
-    // Importer les données des promos
-    // Vérifions si dans le tableau "promos" il existe un objet dont l'ID est promoId 
-    const doesPromoExist = promos.some(promo => promo.id === promoId);
-    if (!doesPromoExist) {
-      next(); // On renvoie la 404
-      return; // Note : on pourrait faire un ELSE au lieu de return. Ceci est une clause de guard / early return
-    }
+    db.query(`SELECT * FROM "student" WHERE promo_id = ${promoId}`)
 
-    // Récupérer les étudiants qui sont dans la promo.
-    const promoStudents = students.filter(student => student.promo === promoId);
+      .then(result => {
+        if (result.rows.length === 0) {next();  return;}
 
-    // Renvoyer la page qui affiche les étudiants
-    // 1. Créer la vue EJS
-    // 2. Fournir les étudiants à la vue pour qu'elle puisse faire une boucle dessus
-    // 3. implémenter la vue !
+        const promoStudents = result.rows;
 
-    res.render("students", { students: promoStudents });
+        res.render("students", { students: promoStudents });
+      })
+      .catch(error => {
+        res.status(404).render("500");
+      });
+
   },
 
   renderOneStudentPage(req, res, next) {
