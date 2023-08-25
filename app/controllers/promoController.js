e5775aa7840f803417c674a8be34fed4a0261c0b
KimberleyBonix <kimberley.bonix@oclock.school>
2023-08-26 00:29:34 +0200

    :recycle: refactor: passing all promess in async function

--- app/controllers/promoController.js
+++ app/controllers/promoController.js
@@ -1,47 +1,34 @@
 const db = require("../database/client");
 
 const promoController = {
-  renderAllPromosPage(req, res) {
+  renderAllPromosPage: async(req, res) => {
+    try {
+      const promoResult = await db.query('SELECT * FROM "promo" ORDER BY name ASC');
+      const promos = promoResult.rows;
+      res.render("promos", { promos });
 
-    db.query('SELECT * FROM "promo" ORDER BY name ASC') // Promesse = un objet dont le resultat arrivera "plus tard"
-      
-      .then(result => { // Accéder au resultat de la promesse lorsqu'elle est 'fulfilled' // EX : result = { rows: [{}, {}]}
-        const promos = result.rows;
-        res.render("promos", { promos });
-      })
-
-      .catch(error => { // Accéder à l'erreur de la promesse si elle est 'rejected'
-        console.error(error);
-        res.status(500).render("500"); // 500 car la BDD plante, c'est pas la faute du client !
-      });
+    } 
+    catch (error){
+      res.status(404).render("500");
+    }
 
   },
 
-  renderOnePromoPage(req, res, next) {
-    // Controle des inputs utilisateurs 
-    const promoId = parseInt(req.params.id);
-    if (isNaN(promoId)) { return next(); /* 404 */ }
+  renderOnePromoPage: async(req, res, next) => {
+    const promoId = Number(req.params.id);
+    if (isNaN(promoId)) { return next();}
 
-    // Query à la BDD
-    db.query(`SELECT * FROM "promo" WHERE id = $1`, [promoId])
-    
-      .then(result => {
-        // Gestion 404
-        if (result.rows.length === 0) { // Si le tableau des resultats est vide, c'est que la promo n'existe pas !
-          next(); return; // 404 : le client n'est pas sensé demander cette ressource. (Not Found !)
-        }
-        // Gestion du succes de la query
-        const promo = result.rows[0]; // Attention : result = { rows: [ {...}, {...}, {...} ] }
-        // console.log(result); // { command: 'SELECT', rows: [{ id: 663, name: 'Cheesecake', github_organization: 'https://github.com/O-clock-Cheesecake' } ] }
-        
-        // Render la bonne page
-        res.render("promo", { promo });
-      })
+    try {
+      const promoResult = await db.query(`SELECT * FROM "promo" WHERE id = $1`, [promoId]);
+      if (promoResult.rows.length === 0) { next(); return; }
 
-      .catch(error => {
-        console.error(error);
-        res.status(500).render("500");
-      });
+      const promo = promoResult.rows[0]; 
+      res.render("promo", { promo });
+    } 
+    catch (error){
+      res.status(404).render("500");
+    }
+
   }
 };
 
--- app/controllers/studentController.js
+++ app/controllers/studentController.js
@@ -1,62 +1,41 @@
 const db = require('../database/client');
 
 const studentController = {
-  /* renderAllStudentsOfPromoPage(req, res, next) {
+  renderAllStudentsOfPromoPage: async(req, res, next) => {
     const promoId = Number(req.params.id);
 
-    db.query(`SELECT * FROM "student" WHERE promo_id = ${promoId}`)
+    try {
+      const promoResult = await db.query(`SELECT * FROM "promo" WHERE id = $1`, [promoId]);
 
-      .then(result => {
-        if (result.rows.length === 0) {next();  return;}
+      const promo = promoResult.rows[0];
+      if (!promo) {next();return;}
 
-        const promoStudents = result.rows;
-        res.render("students", { students: promoStudents, promo });
-      })
-      .catch(error => {
-        res.status(404).render("500");
-      });
+      const studentsResult = await db.query(`SELECT * FROM "student" WHERE promo_id = $1`, [promoId]);
 
-  }, */
+      const promoStudents = studentsResult.rows;
+      res.render("students", { students: promoStudents, promo });
 
-  renderAllStudentsOfPromoPage(req, res, next) {
-    const promoId = Number(req.params.id);
-  
-    // Query to fetch promo information
-    const promoQuery = `SELECT * FROM "promo" WHERE id = $1`;
-  
-    // Query to fetch students of the promo
-    const studentsQuery = `SELECT * FROM "student" WHERE promo_id = $1`;
-  
-    db.query(promoQuery, [promoId])
-      .then(promoResult => {
-        const promo = promoResult.rows[0];
-        
-        if (!promo) {next();return;}
-  
-        db.query(studentsQuery, [promoId])
-          .then(studentsResult => {
-            const promoStudents = studentsResult.rows;
-            res.render("students", { students: promoStudents, promo });
-          });
-      })
-      .catch(error => {
-        res.status(404).render("500");
-      });
+    }
+    catch (error){
+      res.status(404).render("500");
+    }
+
   },
 
-  renderOneStudentPage(req, res, next) {
+  renderOneStudentPage: async (req, res, next) => {
     const studentId = parseInt(req.params.id);
 
-    db.query(`SELECT * FROM "student" WHERE "id" = $1`, [studentId])
-      .then(result => {
-        if (result.rows.length === 0) {next();return;}
+    try {
+      const studentPageResult = await db.query(`SELECT * FROM "student" WHERE "id" = $1`, [studentId]);
 
-        const student = result.rows[0];
-        res.render("student", { student });
-      })
-      .catch(error => {
-        res.status(500).send("500");
-      });
+      if (studentPageResult.rows.length === 0) {next();return;}
+
+      const student = studentPageResult.rows[0];
+      res.render("student", { student});
+    }
+    catch(error) {
+      res.status(500).send("500");
+    }
   }
 };
 
