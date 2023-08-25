d01b4c4d073d65f0d7cb7d62b77dcd3627cd8200
KimberleyBonix <kimberley.bonix@oclock.school>
2023-08-26 00:03:06 +0200

    :lock: éviter les injections SQL en param

--- app/controllers/studentController.js
+++ app/controllers/studentController.js
@@ -22,18 +22,18 @@ const studentController = {
     const promoId = Number(req.params.id);
   
     // Query to fetch promo information
-    const promoQuery = `SELECT * FROM "promo" WHERE id = ${promoId}`;
+    const promoQuery = `SELECT * FROM "promo" WHERE id = $1`;
   
     // Query to fetch students of the promo
-    const studentsQuery = `SELECT * FROM "student" WHERE promo_id = ${promoId}`;
+    const studentsQuery = `SELECT * FROM "student" WHERE promo_id = $1`;
   
-    db.query(promoQuery)
+    db.query(promoQuery, [promoId])
       .then(promoResult => {
         const promo = promoResult.rows[0];
         
         if (!promo) {next();return;}
   
-        db.query(studentsQuery)
+        db.query(studentsQuery, [promoId])
           .then(studentsResult => {
             const promoStudents = studentsResult.rows;
             res.render("students", { students: promoStudents, promo });
@@ -47,7 +47,7 @@ const studentController = {
   renderOneStudentPage(req, res, next) {
     const studentId = parseInt(req.params.id);
 
-    db.query(`SELECT * FROM "student" WHERE "id" = ${studentId}`)
+    db.query(`SELECT * FROM "student" WHERE "id" = $1`, [studentId])
       .then(result => {
         if (result.rows.length === 0) {next();return;}
 
